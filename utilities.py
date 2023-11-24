482935524b150146e3b1b788fdb79d3453b8dbdc
Dominik Lehmann <domi.lehma@gmail.com>
2023-11-24 09:51:36 +0100

    update 7

--- utilities.py
+++ utilities.py
@@ -9,3 +9,22 @@ class CohortDateMapper:
         dates = sorted(dates, key= (lambda x : datetime(int(x[0]), int(x[1]), 1)))
 
         self.date_cohort_mapping = {cohort: date for (cohort, date) in zip(range(12), dates)}
+
+    def periods_to_months(self, periods: list):
+        months_in_int = [self.date_cohort_mapping[period][1] for period in periods]
+
+        def int_to_month(number: int):
+            months = [
+            "January", "February", "March", "April",
+            "May", "June", "July", "August",
+            "September", "October", "November", "December"
+            ]
+
+            # Check if the number is within a valid range
+            if 1 <= number <= 12:
+                return months[number - 1]
+            else:
+                return "Invalid month number"
+            
+        return [int_to_month(number) for number in months_in_int]
+
